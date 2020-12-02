2ba9010c554e17dce463154c48a2f8eeffb19141
Music <musicm987@gmail.com>
2020-12-02 12:43:38 -0600

    added bodyparser and a require that was forgotten

--- index.js
+++ index.js
@@ -2,11 +2,33 @@ const express = require('express')
 const app = express()
 
 const knex = require('knex')
-const config = ('./knexfile').development
+const config = require('./knexfile').development
 const database = knex(config)
 
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
+
 app.post('/users', (request, response) => {
-  response.json({user: {id: 1}})
+
+  const { user } = request.body
+
+  database('user')
+    .insert({
+      id: user.id,
+      username: user.username,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      password_hash: user.password
+    })
+    .returning('*')
+    .then( users => {
+      const user = user[0]
+
+      response.json({user: {id: 1}})
+    }).catch(error => {
+      response.json({ error: error.message})
+    })
 })
 
 app.listen(7001)
